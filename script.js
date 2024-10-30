2141505c0f371df51cc0a7bb74821b4bf9e1a358
Sariah Jackson <sarijah13@gmail.com>
2024-10-29 21:02:30 -0400

    new hero

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
 });
+
+// Menu slide in
+const menu = document.getElementById('menu');
+const menuItems = document.getElementById('menu-items');
+
+menu.addEventListener('click', () => {
+    menuItems.style.display = 'block';
+    menuItems.style.opacity = '1';
+});
+
